15acec62faba94202182d2bfe47741f1129a5d02
Giacomo <giacomoiengo308@gmail.com>
2020-02-16 22:41:02 +0100

    Rewritten

--- README.md
+++ README.md
@@ -8,37 +8,27 @@
 
 
 ### SYNOPSIS  
-	#include <stdio.h>  
-	#include <stdlib.h>  
-	int examine(void *data, const int count, const int blocksize, const char format);  
+	void examine(void *data, const int count, const int blocksize, const char format);  
 
 ### DESCRIPTION  
 	The examine() function will help you to examine the memory like the "x" command in gdb.  
-	data		is the pointer to the data block that you want to examine.  
-			It can't be equals to NULL.  
-	count	    	is the number of data blocks that you want to examine consecutively.  
-			It can be whatever integer number (be careful to read out of the segment).  
+	data        is the pointer to the data block that you want to examine.  
+
+	count       is the number of data blocks that you want to examine consecutively.  
+	            It can be whatever integer number (be careful to read out of the segment).  
 	blocksize	is the size, in byte, of the data block.  
-			It can be equals to 1,2,4,8.  
+	            It can be equal to 1,2,4 or 8.  
 	format  	is the format of the output that you want to visualize the data block.  
-			It can be equals to 'd'(signed decimal), 'u'(unsigned decimal), 'x'(hexadecimal),
-			'b'(binary), 'o'(octal), 'c'(ascii char).   
+			    It can be equal to 'd'(signed decimal), 'u'(unsigned decimal), 'x'(hexadecimal),
+		    	'b'(binary), 'o'(octal), 'c'(ascii char).   
 	
-
-### RETURN VALUE  
-	1.	Invalid data pointer.  
-	2.	Invalid blocksize.  
-	3.	Invalid format.  
-	
-
 ### EXAMPLE  
 	To print an int value in its four bytes, in binary format:
 		int a = 0xAABBCCDD;  
 		examine((void*) &a, 4, sizeof(char), 'b'); 
 	The output:
-		0x7ffd799d513c:	11011101	11001100  	
-		0x7ffd799d513e:	10111011	10101010  
-
-		
-		
+        0000005E2F3AF740:       11011101
+        0000005E2F3AF741:       11001100
+        0000005E2F3AF742:       10111011
+        0000005E2F3AF743:       10101010		
 
--- examine.c
+++ examine.c
@@ -1,162 +1,199 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 
-int examine(void *data, const int count, const int blocksize, const char format){
-	
-	int i,j;
-	void **addresses;
-	char **binStrings;
+
+char *hexByte =		"%02hhx";
+char *decByte =		"%03hhd";
+char *udecByte =	"%03hhu";
+char *octByte =		"%03hho";
+char *asciiByte =	"%c";
+
+char *hexWord =		"%04hx";
+char *decWord =		"%05hd";
+char *udecWord =	"%05hu";
+char *octWord =		"%06ho";
+
+char *hexDword =	"%08x";
+char *decDword =	"%010d";
+char *udecDword =	"%010u";
+char *octDword =	"%011o";
+
+char *hexQword =	"%016lx";
+char *decQword =	"%020ld";
+char *udecQword =	"%020lu";
+char *octQword = 	"%022lo";
+
+void bin(void *data, const int size)
+{
+	int i, j;
+	unsigned char *ptr;
+	ptr = (unsigned char*) data;
+
+	for (i=size-1; i >= 0; i--)
+		for (j=7; j >= 0; j--)
+			printf("%u", (ptr[i] >> j) & 1);
+}
+
+void examine1(void *data, const int count, const char format)
+{
 	char *fmt;
+	uint32_t i;
+	int8_t *ptr = (int8_t*) data;
 
-	char *binValue = 	"%s";
-	char *hexByte =		"%02hhx";
-	char *decByte =		"%03hhd";
-	char *udecByte =	"%03hhu";
-	char *octByte =		"%03hho";
-	char *asciiByte =	"%c";
-	
-	char *hexWord =		"%04hx";
-	char *decWord =		"%05hd";
-	char *udecWord =	"%05hu";
-	char *octWord =		"%06ho";
-	
-	char *hexDword =	"%08x";
-	char *decDword =	"%010d";
-	char *udecDword =	"%010u";
-	char *octDword =	"%011o";
-	
-	char *hexQword =	"%016lx";
-	char *decQword =	"%020ld";
-	char *udecQword =	"%020lu";
-	char *octQword = 	"%022lo";
-	
-	
-	
-	// Checking arguments 
-	if (data == NULL) return 1;
-	
-	if (blocksize != sizeof(char) && blocksize != sizeof(short) &&
-		blocksize != sizeof(int)  && blocksize != sizeof(long)) return 2;
-	
-	if (format == 'c' && blocksize != sizeof(char) ) return 2;
-	
-	if (format != 'd' && format != 'u' && format != 'o' && 
-		format != 'b' && format != 'c' && format != 'x') return 3;
-		
-	
-	// Mallocating space
-	addresses = malloc(count * sizeof(void*));
-	binStrings = malloc(count * sizeof(void*));
-	for (i = 0; i < count; i++)
-		binStrings[i] = malloc((blocksize*8)+1);
-	
-	
-	
-	// Setting appropriate format-string
-	// Storing addresses of data that will be examined
-	switch(blocksize){
-		case sizeof(char):
-			for (i = 0; i < count; i++) addresses[i] = ((char*)data)+i;
-			switch(format){
-				case 'x':
-					fmt = hexByte; break;
-				case 'd':
-					fmt = decByte; break;
-				case 'u':
-					fmt = udecByte; break;
-				case 'o':
-					fmt = octByte; break;
-				case 'c':	
-					fmt = asciiByte; break;
-				case 'b':
-					fmt = binValue;
-					for (i = 0; i < count; i++)
-						for (j = 7; j >= 0; j--, *(((unsigned char*) data) + i) /= 2)
-							binStrings[i][j] = ( *(((unsigned char*) data) + i) %  2) + '0';
-					break;
-			} break;
 
-		case sizeof(short):
-			for (i = 0; i < count; i++) addresses[i] = ((short*)data)+i;
-			switch(format){
-				case 'x':
-					fmt = hexWord; break;
-				case 'd':
-					fmt = decWord; break;
-				case 'u':
-					fmt = udecWord; break;
-				case 'o':
-					fmt = octWord; break;	
-				case 'b':
-					fmt = binValue;
-					for (i = 0; i < count; i++)
-						for (j = 15; j >= 0; j--, *(((unsigned short*) data) + i) /= 2)
-							binStrings[i][j] = ( *(((unsigned short*) data) + i) %  2) + '0';
-					break;
-			} break;
-		
-		case sizeof(int):
-			for (i = 0; i < count; i++) addresses[i] = ((int*)data)+i;
-			switch(format){
-				case 'x':
-					fmt = hexDword; break;
-				case 'd':
-					fmt = decDword; break;
-				case 'u':
-					fmt = udecDword; break;
-				case 'o':
-					fmt = octDword; break;
-				case 'b':
-					fmt = binValue;
-					for (i = 0; i < count; i++)
-						for (j = 31; j >= 0; j--, *(((unsigned int*) data) + i) /= 2)
-							binStrings[i][j] = ( *(((unsigned int*) data) + i) %  2) + '0';
-					break;
-			} break;
-		
-		case sizeof(long):
-			for (i = 0; i < count; i++) addresses[i] = ((long*)data)+i;
-			switch(format){
-				case 'x':
-					fmt = hexQword; break;
-				case 'd':
-					fmt = decQword; break;
-				case 'u':
-					fmt = udecQword; break;
-				case 'o':
-					fmt = octQword; break;
-				case 'b':
-					fmt = binValue;
-					for (i = 0; i < count; i++)
-						for (j = 63; j >= 0; j--, *(((unsigned long*) data) + i) /= 2)
-							binStrings[i][j] = ( *(((unsigned long*) data) + i) %  2) + '0';
-					break;
-			} break;
-							
+	switch(format){
+		case 'b':
+			break;
+		case 'x':
+			fmt = hexByte; break;
+		case 'd':
+			fmt = decByte; break;
+		case 'u':
+			fmt = udecByte; break;
+		case 'o':
+			fmt = octByte; break;
+		case 'c':	
+			fmt = asciiByte; break;
+		default:
+			return;
 	}
-	
-	
-	
-	// Print loop
-	for (i = 0; i < count; i++){
+
+	for(i=0; i<count; i++)
+	{
+		printf("%p:\t", ptr+i);
 		
-		// For formatting reasons
-		if (i%2 == 0)
-			(i != 0) ? printf("\n%p:\t",addresses[i]) : printf("%p:\t",addresses[i]);
-	
-		// If format is binary, then print the strings stored in `binStrings`
-		// else, print values pointed by the addresses stored in `addresses`
-		(format != 'b') ? printf(fmt,**((long**)addresses+i)) : printf(fmt,binStrings[i]);
-		printf("\t");
-	}	
-	
-	
-	
-	// Deallocate space
-	for (i = 0; i < count; i++)
-		free(binStrings[i]);
-	free(binStrings);
-	free(addresses);
-	return 0;
+		if (format == 'b')
+			bin(ptr+i, sizeof(int8_t));
+		else
+			printf(fmt, *(ptr+i));
+		
+		printf("\n");
+	}
+
+}
+
+void examine2(void *data, const int count, const char format)
+{
+	char *fmt;
+	uint32_t i;
+	int16_t *ptr = (int16_t*) data;
+
+
+	switch(format){
+		case 'b':
+			break;
+		case 'x':
+			fmt = hexWord; break;
+		case 'd':
+			fmt = decWord; break;
+		case 'u':
+			fmt = udecWord; break;
+		case 'o':
+			fmt = octWord; break;
+		default:
+			return;
+	}
+
+	for(i=0; i<count; i++)
+	{
+		printf("%p:\t", ptr+i);
+		
+		if (format == 'b')
+			bin(ptr+i, sizeof(int16_t));
+		else
+			printf(fmt, *(ptr+i));
+		
+		printf("\n");
+	}
+}
+
+void examine4(void *data, const int count, const char format)
+{
+	char *fmt;
+	uint32_t i;
+	int32_t *ptr = (int32_t*) data;
+
+
+	switch(format){
+		case 'b':
+			break;
+		case 'x':
+			fmt = hexDword; break;
+		case 'd':
+			fmt = decDword; break;
+		case 'u':
+			fmt = udecDword; break;
+		case 'o':
+			fmt = octDword; break;
+		default:
+			return;
+	}
+
+	for(i=0; i<count; i++)
+	{
+		printf("%p:\t", ptr+i);
+		
+		if (format == 'b')
+			bin(ptr+i, sizeof(int32_t));
+		else
+			printf(fmt, *(ptr+i));
+		
+		printf("\n");
+	}
+
+}
+
+void examine8(void *data, const int count, const char format)
+{
+	char *fmt;
+	uint32_t i;
+	int64_t *ptr = (int64_t*) data;
+
+
+	switch(format){
+		case 'b':
+			break;
+		case 'x':
+			fmt = hexQword; break;
+		case 'd':
+			fmt = decQword; break;
+		case 'u':
+			fmt = udecQword; break;
+		case 'o':
+			fmt = octQword; break;
+		default:
+			return;
+	}
+
+	for(i=0; i<count; i++)
+	{
+		printf("%p:\t", ptr+i);
+		
+		if (format == 'b')
+			bin(ptr+i, sizeof(int64_t));
+		else
+			printf(fmt, *(ptr+i));
+		
+		printf("\n");
+	}
+
+}
+
+void examine(void *data, const int count, const int blocksize, const char format)
+{
+
+	switch(blocksize){
+		case sizeof(int8_t):
+			examine1(data, count, format); break;
+
+		case sizeof(int16_t):
+			examine2(data, count, format); break;
+
+		case sizeof(int32_t):
+			examine4(data, count, format); break;
+
+		case sizeof(int64_t):
+			examine8(data, count, format); break;
+	}
 
 }
